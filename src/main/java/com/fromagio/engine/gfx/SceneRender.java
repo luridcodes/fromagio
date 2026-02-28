2f9c4262219bef134614d02e0dee5ada4717c395
lucas_LOQ <lucas.t.sgp@gmail.com>
2026-02-28 12:30:56 +0800

    move shaders to src/

--- src/main/java/com/fromagio/engine/gfx/SceneRender.java
+++ src/main/java/com/fromagio/engine/gfx/SceneRender.java
@@ -23,8 +23,8 @@ public class SceneRender {
 
     public SceneRender() {
         List<ShaderProgram.ShaderModuleData> shaderModuleDataList = new ArrayList<>();
-        shaderModuleDataList.add(new ShaderProgram.ShaderModuleData("resources/shaders/scene.vert", GL_VERTEX_SHADER));
-        shaderModuleDataList.add(new ShaderProgram.ShaderModuleData("resources/shaders/scene.frag", GL_FRAGMENT_SHADER));
+        shaderModuleDataList.add(new ShaderProgram.ShaderModuleData("src/resources/shaders/scene.vert", GL_VERTEX_SHADER));
+        shaderModuleDataList.add(new ShaderProgram.ShaderModuleData("src/resources/shaders/scene.frag", GL_FRAGMENT_SHADER));
         shaderProgram = new ShaderProgram(shaderModuleDataList);
         shaderProgram.validate();
 
